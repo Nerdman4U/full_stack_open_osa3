b7ec97387f647a143f908481305a610ebfb946c7
Joni Toyryla <info@jonitoyryla.eu>
2023-10-16 12:36:34 +0300

    Osa 3
    Tehtävä 1
    Poistettu lisäksi ylimääräinen nodeman ja lisätty tarpeellinen nodemon...

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const PORT = 3001
+
+const app = express()
+app.use(express.json())
+
+const persons = [
+    {id: 1, name: "asdf1", number:1},
+    {id: 2, name: "asdf2", number:2},
+    {id: 3, name: "asdf3", number:3},
+    {id: 4, name: "asdf4", number:4},
+    {id: 5, name: "asdf5", number:5},
+    {id: 5, name: "asdf6", number:6}
+]
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+  
+
+
+app.listen(PORT, () => {
+    console.log("Palvelin pyörimässä!")
+})
